f508bfabd2ad509a98612bed3cdb3d72bca04df1
alpysbaymagzhan12-web <alpysbaymagzhan12@gmail.com>
2026-02-27 21:26:13 +0500

    Add Practice4 - Python iterators, generators, dates, math, and JSON

---
+++ Practice 4/Python datatime Module.py
@@ -0,0 +1,12 @@
+from datetime import timedelta
+delta = timedelta(
+    days=50,
+    seconds=27,
+    microseconds=10,
+    milliseconds=29000,
+    minutes=5,
+    hours=8,
+    weeks=2
+)
+# Only days, seconds, and microseconds remain
+delta
